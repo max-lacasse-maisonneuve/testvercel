ebae3c51a86bf6170474df4f58a4c4ad674ba9c8
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-03-26 10:48:29 -0400

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    @vite(['resources/js/app.js'])
+    <script src="{{ Vite::asset('resources/js/app.js') }}"></script>
     <!-- Styles -->
     <style>
         /* ! tailwindcss v3.4.1 | MIT License | https://tailwindcss.com */
